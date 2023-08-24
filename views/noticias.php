96ec4d4410d0aeb2be08bc3e4863c4cca553a33e
João Pegoraro <hahmraro@tuta.io>
2023-08-24 14:59:34 -0300

    fixed table sticky behaviour

--- views/noticias.php
+++ views/noticias.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Meu Mengão - Agregador de notícias do Flamengo</title>
     <link rel="stylesheet" href="css/noticias.css">
 </head>
@@ -18,7 +19,7 @@
                         <li><a class="main-nav-link" href="/calendario">Calendário</a></li>
                         <li><a class="main-nav-link" href="/tabelas">Tabelas</a></li>
                     </ul>
-                    <button class="main-nav-app-button">Baixe nosso aplicativo</button>
+                    <button class="main-nav-app-button">Baixe o aplicativo</button>
                 </nav>
             </div>
         </div>
@@ -26,26 +27,24 @@
 
     <main>
         <div class="container">
-            <div class="main-columns">
+            <div class="main">
                 <section class="article-list | flow" style="--flow-space: 2em">
                     <h1 class="fs-primary-heading fw-bold">Notícias</h1>
                     <ul class="flow" style="--flow-space: 3em" role="list" aria-label="Notícias">
                         <?php for ($i = 0; $i < 10; $i++) : ?>
-                        <li>
-                            <article>
-                                <a class="article-link" href=<?= '/' . $i ?>>
-                                    <img class="article-image"
-                                        src="https://colunadofla.com/wp-content/uploads/2023/08/elenco-time-jogadores-poster-flamengo-brasileirao.jpg"
-                                        alt="Time do flamengo entrando em campo">
-                                    <div class="article-content">
-                                        <p>Globo Esporte</p>
-                                        <h2 class="fw-bold fs-secondary-heading" style="--flow-space: 0.5rem">
-                                            Flamengo escolhe Cariacica para jogar contra o Athletico
-                                        </h2>
-                                    </div>
-                                </a>
-                            </article>
-                        </li>
+                            <li>
+                                <article>
+                                    <a class="article-link" href=<?= '/' . $i ?>>
+                                        <img class="article-image" src="https://colunadofla.com/wp-content/uploads/2023/08/elenco-time-jogadores-poster-flamengo-brasileirao.jpg" alt="Time do flamengo entrando em campo">
+                                        <div class="article-content">
+                                            <p>Globo Esporte</p>
+                                            <h2 class="fw-bold fs-secondary-heading" style="--flow-space: 0.5rem">
+                                                Flamengo escolhe Cariacica para jogar contra o Athletico
+                                            </h2>
+                                        </div>
+                                    </a>
+                                </article>
+                            </li>
                         <?php endfor ?>
                     </ul>
                 </section>
@@ -54,7 +53,7 @@
                     <section class="next-match-section | flow" style="--flow-space: 1em">
                         <h2 class="fs-primary-heading fw-bold">Próxima Partida</h2>
                         <div class="next-match-card">
-                            <div class="next-match-card-content">
+                            <div class="next-match-card-content | flow" style="--flow-space: 1em">
                                 <p>Flamengo</p>
                                 <p>Vasco</p>
                             </div>
@@ -68,21 +67,20 @@
                     <section class="table-section">
                         <h2 class="fs-primary-heading fw-bold">Tabela Brasileirão</h2>
                         <table>
-                            <tr class="table-header-row">
-                                <th>#</th>
-                                <th>Classificação</th>
-                                <th>P</th>
-                                <th>J</th>
-                                <th>V</th>
+                            <tr>
+                                <th class="position-header" colspan="3">Classificação</th>
+                                <th class="points-header">P</th>
+                                <th class="games-header">J</th>
+                                <th class="wins-header">V</th>
                             </tr>
                             <?php for ($pos = 0; $pos < 10; $pos++) : ?>
-                            <tr class="table-body-row">
-                                <th><?= $pos ?></th>
-                                <th>Time <?= $pos ?></th>
-                                <th><?= (15 - $pos) * 2 ?></th>
-                                <th>15</th>
-                                <th><?= 15 - $pos ?></th>
-                            </tr>
+                                <tr>
+                                    <td class="position"><?= $pos + 1 ?></td>
+                                    <td class="team-name" colspan="2">Red Bull Bragantino <?= $pos ?></td>
+                                    <td class="points"><?= (15 - $pos) * 2 ?></td>
+                                    <td class="games">15</td>
+                                    <td class="wins"><?= 15 - $pos ?></td>
+                                </tr>
                             <?php endfor ?>
                         </table>
                     </section>
