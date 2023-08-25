a5dd455507b2f23f25d33275704aca8585c2f650
João Pegoraro <hahmraro@tuta.io>
2023-08-25 18:49:19 -0300

    moved table css from noticias.css to tabela.css

--- views/components/tabela.php
+++ views/components/tabela.php
@@ -9,21 +9,20 @@ use App\Model\Posicao;
 
 <table>
     <tr>
-        <th class="position-header" colspan="3">Classificação</th>
-        <th class="points-header">P</th>
-        <th class="games-header">J</th>
-        <th class="wins-header">V</th>
+        <th class="table-position-header" colspan="3">Classificação</th>
+        <th class="table-points-header">P</th>
+        <th class="table-games-header">J</th>
+        <th class="table-wins-header">V</th>
     </tr>
     <?php foreach ($posicoes as $posicao) : ?>
-    <tr>
-        <td class="table-position"><?= htmlspecialchars($posicao->posicao) ?></td>
-        <td class="table-team-name" colspan="2"
-            style="--icon-url: url('<?= htmlspecialchars($posicao->escudoTime) ?>');">
-            <?= htmlspecialchars($posicao->nomeTime) ?>
-        </td>
-        <td class="table-points"><?= htmlspecialchars($posicao->pontos) ?></td>
-        <td class="table-games"><?= htmlspecialchars($posicao->jogos) ?></td>
-        <td class="table-wins"><?= htmlspecialchars($posicao->vitorias) ?></td>
-    </tr>
+        <tr>
+            <td class="table-position"><?= htmlspecialchars($posicao->posicao) ?></td>
+            <td class="table-team-name" colspan="2" style="--icon-url: url('<?= htmlspecialchars($posicao->escudoTime) ?>');">
+                <?= htmlspecialchars($posicao->nomeTime) ?>
+            </td>
+            <td class="table-points"><?= htmlspecialchars($posicao->pontos) ?></td>
+            <td class="table-games"><?= htmlspecialchars($posicao->jogos) ?></td>
+            <td class="table-wins"><?= htmlspecialchars($posicao->vitorias) ?></td>
+        </tr>
     <?php endforeach ?>
 </table>
