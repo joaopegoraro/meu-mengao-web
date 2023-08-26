c32c0f7483f01f89493b9b2cda420edd4547ad42
João Pegoraro <hahmraro@tuta.io>
2023-08-26 13:59:07 -0300

    started making the tables page ui

--- views/components/tabela.php
+++ views/components/tabela.php
@@ -3,9 +3,14 @@
 use App\Model\Posicao;
 
 /**
+ * @var string $campeonatoName;
  * @var Posicao[] $posicoes
  */
 ?>
+
+<?php if ($campeonatoName) : ?>
+<h2 class="fs-secondary-heading fw-bold"><?= $campeonatoName ?></h2>
+<?php endif ?>
 
 <table>
     <tr>
@@ -15,14 +20,15 @@ use App\Model\Posicao;
         <th class="table-wins-header">V</th>
     </tr>
     <?php foreach ($posicoes as $posicao) : ?>
-        <tr>
-            <td class="table-position"><?= htmlspecialchars($posicao->posicao) ?></td>
-            <td class="table-team-name" colspan="2" style="--icon-url: url('<?= htmlspecialchars($posicao->escudoTime) ?>');">
-                <?= htmlspecialchars($posicao->nomeTime) ?>
-            </td>
-            <td class="table-points"><?= htmlspecialchars($posicao->pontos) ?></td>
-            <td class="table-games"><?= htmlspecialchars($posicao->jogos) ?></td>
-            <td class="table-wins"><?= htmlspecialchars($posicao->vitorias) ?></td>
-        </tr>
+    <tr>
+        <td class="table-position"><?= htmlspecialchars($posicao->posicao) ?></td>
+        <td class="table-team-name" colspan="2"
+            style="--icon-url: url('<?= htmlspecialchars($posicao->escudoTime) ?>');">
+            <?= htmlspecialchars($posicao->nomeTime) ?>
+        </td>
+        <td class="table-points"><?= htmlspecialchars($posicao->pontos) ?></td>
+        <td class="table-games"><?= htmlspecialchars($posicao->jogos) ?></td>
+        <td class="table-wins"><?= htmlspecialchars($posicao->vitorias) ?></td>
+    </tr>
     <?php endforeach ?>
 </table>
--- views/tabelas.php
+++ views/tabelas.php
@@ -1,3 +1,45 @@
-<main>
+<?php
 
+use App\Model\Campeonato;
+use App\Model\Partida;
+use App\Model\Posicao;
+
+/**
+ * @var string $campeonatoName
+ * @var Campeonato[] $campeonatos
+ * @var mixed $rodadaViews As views que representam as rodadas
+ * @var mixed $tabelaViews As views que representam as tabelas
+ */
+?>
+
+<main>
+    <div class="container">
+        <div class="main">
+            <section class="championship-tables">
+                <div class="tables-dropdown" tabindex=0>
+                    <h1 class="tables-dropdown-title | fs-primary-heading fw-bold"><?= $campeonatoName ?? 'Campeonato' ?>
+                    </h1>
+                    <div class="tables-dropdown-content">
+                        <?php foreach ($campeonatos as $campeonato) : ?>
+                            <a class="tables-dropdown-item" href="<?= $campeonato->id ?>"><?= $campeonato->nome ?></a>
+                        <?php endforeach ?>
+                    </div>
+                    <div class="table-list">
+                        <?php foreach ($tabelaViews as $tabelaView) : ?>
+                            <?= $tabelaView ?>
+                        <?php endforeach ?>
+                    </div>
+                </div>
+            </section>
+
+            <section class="championship-rounds">
+                <h1 class="fs-primary-heading fw-bold">Rodadas</h1>
+                <div class="round-list">
+                    <?php foreach ($rodadaViews as $rodadaView) : ?>
+                        <?= $rodadaView ?>
+                    <?php endforeach ?>
+                </div>
+            </section>
+        </div>
+    </div>
 </main>
