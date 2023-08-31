b9c05dd9431e3a9911c536f0ca3b17ac6fe80e95
João Pegoraro <hahmraro@tuta.io>
2023-08-31 13:11:01 -0300

    added favicon

--- views/base.php
+++ views/base.php
@@ -14,6 +14,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Meu Mengão - <?= $title ? htmlspecialchars($title) : "Agregador de notícias do Flamengo" ?></title>
+    <link rel="icon" type="image/x-icon" href="icons/favicon.ico">
     <link rel="stylesheet" href="css/base.css">
     <?php foreach ($styles as $style) : ?>
         <link rel="stylesheet" href="css/<?= htmlspecialchars($style) ?>.css">
