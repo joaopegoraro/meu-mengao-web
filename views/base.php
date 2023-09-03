1d90f11220e3787d3688a4deacf37f6ecfd34148
João Pegoraro <hahmraro@tuta.io>
2023-09-03 11:24:38 -0300

    refactored base.php and base.css to use bem naming

--- views/base.php
+++ views/base.php
@@ -18,28 +18,26 @@
 
     <title>Meu Mengão - <?= $title ? htmlspecialchars($title) : "Agregador de notícias do Flamengo" ?></title>
 
-    <link rel="stylesheet" href="css/base.css?v=1">
+    <link rel="stylesheet" href="css/style.css?v=1">
 
     <link rel="preload" fetchpriority="low" as="image" href="images/logo-200.webp" type="image/webp">
     <link rel="preload" fetchpriority="low" as="image" href="images/playstore-25.webp" type="image/webp">
+
+    <link rel="icon" type="image/x-icon" href="icons/favicon.ico?v=1">
 </head>
 
 <body>
-    <header role="banner">
-        <div class="container">
-            <div class="main-nav-wrapper | padding-block-600">
-                <nav class="main-nav | fw-bold" aria-label="Navegação principal">
-                    <ul role="list" class="main-nav-list">
-                        <li><a class="main-nav-link" href="/noticias">Notícias</a></li>
-                        <li><a class="main-nav-link" href="/resultados">Resultados</a></li>
-                        <li><a class="main-nav-link" href="/calendario">Calendário</a></li>
-                        <li><a class="main-nav-link" href="/tabelas">Tabelas</a></li>
-                    </ul>
-                    <a href="https://play.google.com/store/apps/details?id=com.joaopegoraro.meu_mengao" target="_blank"
-                        class="main-nav-app-button">Baixe o aplicativo</a>
-                </nav>
-            </div>
-        </div>
+    <header class="l-container" role="banner">
+        <nav class="c-main-nav" aria-label="Navegação principal">
+            <ul class="c-main-nav__list" role="list">
+                <li><a class="anchor--light" href="/noticias">Notícias</a></li>
+                <li><a class="anchor--light" href="/resultados">Resultados</a></li>
+                <li><a class="anchor--light" href="/calendario">Calendário</a></li>
+                <li><a class="anchor--light" href="/tabelas">Tabelas</a></li>
+            </ul>
+            <a class="c-main-nav__button" href="https://play.google.com/store/apps/details?id=com.joaopegoraro.meu_mengao" target="_blank">Baixe o
+                aplicativo</a>
+        </nav>
     </header>
 
     <?php
@@ -49,25 +47,16 @@
     include($content . '.php')
     ?>
 
-    <footer>
-        <div class="container">
-            <nav>
-                <ul class="footer-nav | fw-bold" role="list" aria-label="Informações do site">
-                    <li><a class="footer-nav-link" href="https://github.com/joaopegoraro" target="_blank">Contato</a>
-                    </li>
-                    <li><a class="footer-nav-link" href="https://github.com/joaopegoraro/meu-mengao-web" target="_blank"">Código fonte</a> </li>
-                    <li><a class=" footer-nav-link"
-                            href="https://play.google.com/store/apps/details?id=com.joaopegoraro.meu_mengao"
-                            target="_blank">Aplicativo</a></li>
-                </ul>
-            </nav>
-        </div>
+    <footer class="l-container">
+        <nav>
+            <ul class="c-footer-nav" role="list" aria-label="Informações do site">
+                <li><a class="anchor--dark" href="https://github.com/joaopegoraro" target="_blank">Contato</a></li>
+                <li><a class="anchor--dark" href="https://github.com/joaopegoraro/meu-mengao-web" target="_blank">Código
+                        fonte</a> </li>
+                <li><a class="anchor--dark" href="https://play.google.com/store/apps/details?id=com.joaopegoraro.meu_mengao" target="_blank">Aplicativo</a></li>
+            </ul>
+        </nav>
     </footer>
 </body>
-
-<link rel="icon" type="image/x-icon" href="icons/favicon.ico?v=1">
-<?php foreach ($styles as $style) : ?>
-<link rel="stylesheet" href="css/<?= htmlspecialchars($style) ?>.css?v=1">
-<?php endforeach ?>
 
 </html>
