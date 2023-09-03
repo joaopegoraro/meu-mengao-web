6cc6a5e1beb5d11879edaeb74537897b4049e2ea
João Pegoraro <hahmraro@tuta.io>
2023-09-03 12:12:28 -0300

    refactored noticias.php and noticias.css to use bem naming

--- views/components/partida.php
+++ views/components/partida.php
@@ -10,29 +10,29 @@ use App\Model\Partida;
 ?>
 
 <div class="c-match-card">
-    <div class="c-match-card__content | l-flow">
+    <div class="c-match-card__content">
         <div class="c-match-card__team" style="--icon-url: url('data:image/png;base64,<?= $partida->escudoCasa ?>');">
             <p class="c-match-card__team-name"><?= $partida->timeCasa ?></p>
             <?php if ($mostrarPlacar) : ?>
-            <p><?= $partida->golsCasa ?></p>
+                <p><?= $partida->golsCasa ?></p>
             <?php endif ?>
         </div>
         <div class="c-match-card__team" style="--icon-url: url('data:image/png;base64,<?= $partida->escudoFora ?>');">
             <p class="c-match-card__team-name"><?= $partida->timeFora ?></p>
             <?php if ($mostrarPlacar) : ?>
-            <p><?= $partida->golsFora ?></p>
+                <p><?= $partida->golsFora ?></p>
             <?php endif ?>
         </div>
     </div>
     <div class="c-match-card__footer <?= $esconderCampeonato ? 'c-match-card__footer--center' : '' ?>">
         <?php if (!$esconderCampeonato) : ?>
-        <div class="c-match-card__round-wrapper">
-            <p class="c-match-card__round">
-                <?= $partida->campeonato ?>
-                <br />
-                <?= $partida->rodadaName ?>
-            </p>
-        </div>
+            <div class="c-match-card__round-wrapper">
+                <p class="c-match-card__round">
+                    <?= $partida->campeonato ?>
+                    <br />
+                    <?= $partida->rodadaName ?>
+                </p>
+            </div>
         <?php endif ?>
 
         <div class="c-match-card__date-wrapper">
--- views/noticias.php
+++ views/noticias.php
@@ -11,56 +11,51 @@ use App\Model\Posicao;
  */
 ?>
 
-<main>
-    <div class="container">
-        <div class="main">
-            <section class="article-list | flow" style="--flow-space: 2em">
-                <h1 class="fs-primary-heading fw-bold">Notícias</h1>
-                <ul class="flow" style="--flow-space: 3em" role="list" aria-label="Notícias">
-                    <?php foreach ($noticias as $noticia) : ?>
-                    <li>
-                        <article>
-                            <a class="article-link" href="<?= htmlspecialchars($noticia->link) ?>">
-                                <img class="article-image" src="<?= $noticia->foto ?>" alt="" decoding="async">
-                                <div class="article-content | flow" style="--flow-space: 0.2em">
-                                    <p class="article-site | fw-semi-bold"
-                                        style="--icon-url: url('data:image/png;base64,<?= $noticia->logoSite ?>');">
-                                        <?= $noticia->site ?>
-                                    </p>
-                                    <h2 class="article-title | fw-bold fs-secondary-heading">
-                                        <?= $noticia->titulo ?>
-                                    </h2>
-                                </div>
-                            </a>
-                        </article>
-                    </li>
-                    <?php endforeach ?>
-                </ul>
+<main class="l-container">
+    <div class="l-noticias-grid">
+        <section class="l-article-list">
+            <h1>Notícias</h1>
+            <ul class="l-grid" style="--grid-gap: 3rem" role="list" aria-label="Notícias">
+                <?php foreach ($noticias as $noticia) : ?>
+                <li>
+                    <article>
+                        <a class="anchor--light" href="<?= htmlspecialchars($noticia->link) ?>">
+                            <img class="c-article__image" src="<?= $noticia->foto ?>" alt="" decoding="async">
+                            <div class="c-article__content">
+                                <p class="c-article__site"
+                                    style="--icon-url: url('data:image/png;base64,<?= $noticia->logoSite ?>');">
+                                    <?= $noticia->site ?>
+                                </p>
+                                <h2><?= $noticia->titulo ?></h2>
+                            </div>
+                        </a>
+                    </article>
+                </li>
+                <?php endforeach ?>
+            </ul>
+        </section>
+
+        <aside class="l-noticias-aside">
+            <section class="l-next-match-section">
+                <a class="anchor--dark anchor--big" href="/calendario">
+                    Próxima Partida
+                </a>
+                <?php
+                $partida = $proximaPartida;
+                include 'components/partida.php';
+                ?>
             </section>
 
-            <aside class="flow" style="--flow-space: 2em">
-                <section class="next-match-section | flow" style="--flow-space: 1em">
-                    <a href="/calendario" class="main-heading-link | fs-primary-heading fw-bold">
-                        Próxima Partida
-                    </a>
-                    <div class="next-match-card">
-                        <?php
-                        $partida = $proximaPartida;
-                        include 'components/partida.php';
-                        ?>
-                    </div>
-                </section>
-
-                <section class="table-section">
-                    <a href="/tabelas?id=serie-a" class="main-heading-link | fs-primary-heading fw-bold">
-                        Tabela Brasileirão
-                    </a>
-                    <?= $tabelaBrasileirao ?>
-                    <?php
-                    $posicoes = $tabelaBrasileirao;
-                    include 'components/tabela.php';
-                    ?>
-                </section>
-        </div>
+            <section class="l-table-section">
+                <a class="anchor--dark anchor--big" href="/tabelas?id=serie-a">
+                    Tabela Brasileirão
+                </a>
+                <?= $tabelaBrasileirao ?>
+                <?php
+                $posicoes = $tabelaBrasileirao;
+                include 'components/tabela.php';
+                ?>
+            </section>
+    </div>
     </div>
 </main>
