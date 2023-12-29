0998c6c487119640fc1abb148637c6e940ee438e
João Pegoraro <hahmraro@tuta.io>
2023-12-29 20:30:51 -0300

    Fixed problems when no future matches are found

--- views/components/partidas.php
+++ views/components/partidas.php
@@ -15,9 +15,13 @@ if (!isset($mostrarPlacar)) $mostrarPlacar = false;
     <div class="u-flow" style="--flow-space: 2rem">
         <h1><?= $partidasTitle ?></h1>
         <div class="l-partidas-grid">
+            <?php if ($partidas): ?>
             <?php foreach ($partidas as $partida) : ?>
                 <?php include 'partida.php' ?>
             <?php endforeach ?>
+            <?php else: ?>
+                Nenhuma partida encontrada
+            <?php endif ?>
         </div>
     </div>
 </main>
